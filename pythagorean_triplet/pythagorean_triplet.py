9fd325c05c32e36fa901e9d573f571743d6a978c
Gabe Timm <gtimm@repository.parthenonsoftware.com>
2019-05-08 13:58:48 -0700

    Started work on pythagorean-triplets

---
+++ pythagorean_triplet/pythagorean_triplet.py
@@ -0,0 +1,17 @@
+from math import sqrt, pow
+
+def pythagorean_triplet(sum):
+    a = 1
+    b = 2
+    c = 3
+
+    found = False
+
+    while not found:
+        c = (sum - (a + b))
+
+
+
+
+def is_triplet(a, b, c):
+    return pow(c, 2) is pow(a, 2) + pow(b, 2)
